81eeb6a7716ed3e85e6c985ca747d0d97218639c
Yizhe LI(yizli) <yizli@cisco.com>
2016-10-26 16:59:21 +0200

    Update service 'forward' API

--- serviceApp/serviceApp.py
+++ serviceApp/serviceApp.py
@@ -1,13 +1,31 @@
+import os
+import urllib
 import SocketServer
 import BaseHTTPServer
+from multiprocessing import Process
 from datetime import datetime, date
 from urlparse import urlparse
+
+global PORT
+global SERVER_URL
+
+
 
 def getLogTime():
     return date.today().strftime('%Y-%m-%d ') + datetime.now().strftime('%H:%M:%S.%f ')[:-3] + ' '
 
 def process():
-    print(getLogTime() + "Detached Processing  > ")
+    print(getLogTime() + "Detached Processing from  " )
+    resp = urllib.urlopen(SERVER_URL)
+    if (resp.getcode() != 200) :
+        print(getLogTime() + "Status:" + str(resp.getcode()) + ", Body:" + str(resp.read()))
+        print(getLogTime() + SERVER_URL + "is not accessible")
+        # self.respond(SERVER_URL + "is not accessible", 400)
+    else :
+        print(getLogTime() + "Status:" + str(resp.getcode()) + ", Body:" + str(resp.read()))
+        print(getLogTime() + "HTTP GET Req reach to " + SERVER_URL)
+        #self.respond("HTTP GET Req reach to " + SERVER_URL)
+
 
 class MyRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 
@@ -22,19 +40,21 @@ class MyRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 
         # HTTP status processing /status
         elif elem[1].lower() == "status" :
-            response = ("Service OK");
-            self.respond(response)
+            self.respond("Service OK")
 
         # HTTP request processing /eco
         elif elem[1].lower() == "eco" :
-            response = ("Request received");
-            self.respond(response)
+            self.respond("Request received")
+
+        # HTTP request processing /forward
+        elif elem[1].lower() == "forward" :
+            p = Process(target=process, args=())
+
+            p.start()
+            self.respond("Request forwarded")
 
         else :
             self.respond("Unknow request", 400)
-
-        # HTTP forward processing /forward TODO
-
 
         #query_components = dict(qc.split("=") for qc in params.split("&"))
         #print(getLogTime() + "Receive Params URL:" + str(query_components["url"]) +\
@@ -66,7 +86,13 @@ if __name__=='__main__':
     #config.read('cdnAgent.conf')
 
     #PORT = config.get('SETTINGS', 'SERVICE_PORT')
-    PORT = 8001
+    print os.getenv('SERVER_IP', "127.0.0.1")
+    print os.getenv('SERVER_PORT', 80)
+
+    PORT = os.getenv('SELF_PORT', 8001)
+    print PORT
+    SERVER_URL="http://"+os.getenv('SERVER_IP', "127.0.0.1") + ":" + str(os.getenv('SERVER_PORT', 80)) + "/eco"
+    print SERVER_URL
     httpd = SocketServer.TCPServer(("", int(PORT)), MyRequestHandler)
     print(getLogTime() + "service1 at port " + str(PORT))
     try:
